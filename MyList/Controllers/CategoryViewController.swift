19b58dc45474a7cffc150c87a0f9499345f88a54
Don <Don@macbook-pro.local>
2019-02-01 12:20:55 -0800

    Works with Core Data but still needs cleanup

--- MyList/Controllers/CategoryViewController.swift
+++ MyList/Controllers/CategoryViewController.swift
@@ -10,29 +10,106 @@ import UIKit
 import CoreData
 
 class CategoryViewController: UITableViewController {
+    
+    var catArray = [Category]()
+    
+    let context = (UIApplication.shared.delegate as! AppDelegate).persistentContainer.viewContext
 
     override func viewDidLoad() {
         super.viewDidLoad()
 
+        loadCategories()
+        
     }
 
     //Mark: - TableView Datasource Methods
+    //TODO: Declare numberOfRowsInSection here:
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return catArray.count
+    }
     
+    //TODO: Declare cellForRowAtIndexPath here:
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        
+        let cell = tableView.dequeueReusableCell(withIdentifier: "CategoryCell", for: indexPath)
+        
+        cell.textLabel?.text = catArray[indexPath.row].name
+        
+        return cell
+        
+    }
     
+     //Mark: - TableView Delegate Methods
+    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        performSegue(withIdentifier: "goToItem", sender: self)
+    }
+    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        let destinationVC = segue.destination as! MyListViewController
+        
+        if let indexPath = tableView.indexPathForSelectedRow {
+            destinationVC.selectedCategory = catArray[indexPath.row]
+        }
+    }
     
     //Mark: - Data Manipulation Methods
     
+    func saveCategories () {
+        
+        do {
+            try context.save()
+        }   catch {
+            print("Error Saving Category \(error)")
+        }
+        
+        
+        self.tableView.reloadData()
+    }
     
+    
+    func loadCategories() {
+        
+        let request : NSFetchRequest<Category> = Category.fetchRequest()
+        
+        do {
+            catArray = try context.fetch(request)
+        } catch {
+            print("Error loading catArray \(error)")
+        }
+        
+        tableView.reloadData()
+    }
     
     //MARK: - Add New Categories
     
-    
-   
     @IBAction func addButtonPressed(_ sender: UIBarButtonItem) {
+        var textField = UITextField()
+        
+        let alert = UIAlertController(title: "Add New Category", message: "", preferredStyle: .alert)
+        
+        let action =  UIAlertAction(title: "Add category", style: .default) { (action) in
+            // What will happen once the user clicks the add Item button on our UIA;lert
+            
+            let newCategory = Category(context: self.context)
+            newCategory.name = textField.text!
+            
+            self.catArray.append(newCategory)
+            self.saveCategories()
     }
+        alert.addTextField { (alertTextField) in
+            textField = alertTextField
+            textField.placeholder = "Create new category"
+            
+        }
+        
+        alert.addAction(action)
+        
+        
+        present(alert, animated: true, completion: nil)
+        
 }
 
+}
 
-    //Mark: - TableView Delegate Methods
 
 
--- MyList/Controllers/MyListViewController.swift
+++ MyList/Controllers/MyListViewController.swift
@@ -13,14 +13,20 @@ class MyListViewController: UITableViewController {
     
     var itemArray = [Item]()
     
+    var selectedCategory : Category? {
+        didSet {
+            loadItems()
+        }
+    }
+    
     let context = (UIApplication.shared.delegate as! AppDelegate).persistentContainer.viewContext
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        print(FileManager.default.urls(for: .documentDirectory, in: .userDomainMask))
+        //print(FileManager.default.urls(for: .documentDirectory, in: .userDomainMask))
         
-        loadItems()
+        
         
     }
 
@@ -40,18 +46,9 @@ class MyListViewController: UITableViewController {
         
         cell.textLabel?.text = item.title
         
-        //Ternary operator ==>
-        // value = condition ? valueIfTrue : valueIfFalse
-        
         cell.accessoryType = item.done == true ? .checkmark : .none
         
-        // Ternary Operator Line Above Replaces Following If Statement
-/*        if item.done == true {
-            cell.accessoryType = .checkmark
-        }   else {
-            cell.accessoryType = .none
-        }
-*/
+        
         return cell
         
     }
@@ -90,6 +87,7 @@ class MyListViewController: UITableViewController {
             let newItem = Item(context: self.context)
             newItem.title = textField.text!
             newItem.done = false
+            newItem.parentCategory = self.selectedCategory
             self.itemArray.append(newItem)
             self.saveItems()
             
@@ -121,7 +119,15 @@ class MyListViewController: UITableViewController {
         self.tableView.reloadData()
     }
     
-    func loadItems(with request: NSFetchRequest<Item> = Item.fetchRequest()) {
+    func loadItems(with request: NSFetchRequest<Item> = Item.fetchRequest(), predicate: NSPredicate? = nil) {
+        
+        let categoryPredicate = NSPredicate(format: "parentCategory.name MATCHES %@", selectedCategory!.name!)
+        
+        if let additionalPredicate = predicate {
+            request.predicate = NSCompoundPredicate(andPredicateWithSubpredicates: [categoryPredicate, additionalPredicate])
+        } else {
+            request.predicate = categoryPredicate
+        }
         
         do {
             itemArray = try context.fetch(request)
@@ -140,12 +146,12 @@ extension MyListViewController: UISearchBarDelegate {
     func searchBarSearchButtonClicked(_ searchBar: UISearchBar) {
         let request : NSFetchRequest<Item> = Item.fetchRequest()
         
-        request.predicate = NSPredicate(format: "title CONTAINS[cd] %@", searchBar.text!)
+        let predicate = NSPredicate(format: "title CONTAINS[cd] %@", searchBar.text!)
         
         request.sortDescriptors = [NSSortDescriptor(key: "title", ascending: true)]
         
-        loadItems(with: request)
-        
+        loadItems(with: request, predicate: predicate)
+        //loadItems()
     }
         
         func searchBar(_ searchBar: UISearchBar, textDidChange searchText: String) {
